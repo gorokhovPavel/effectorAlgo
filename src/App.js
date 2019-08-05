42c3c812bffde2535e805d867931cb142045c5c7
gorokhovPavel <pgorokhov@yandex.ru>
2019-08-06 01:05:13 +0300

    add simple api to home project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import RestaurantList from './components/restaurantList/index';
-import {restaurants} from './sources/fixtures'
+import React from "react";
+import RestaurantList from "./components/restaurantList/index";
+//import {restaurants} from './components/restaurantList/index.jsx'
 import "antd/dist/antd.css";
-
 function App() {
-
+  const restaurants = [];
   return (
-    <div>
+    <>
       <RestaurantList dataList={restaurants} />
-    </div>
+    </>
   );
 }
-
 export default App;
--- src/components/restaurantList/index.jsx
+++ src/components/restaurantList/index.jsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import { List, Avatar } from 'antd';
-
-export default function RestaurantList({dataList}){
-
-    console.log(dataList);
-    
-    return(
-        <>
-            <List
-                itemLayout="horizontal"
-                dataSource={dataList}
-                renderItem={item => (
-                    <List.Item style={{ paddingLeft : '8px' }} >
-                        <List.Item.Meta
-                        avatar={<Avatar shape="square" src={item.image} />}
-                        title={item.name}
-                        />
-                    </List.Item>
-                )}
+import React from "react";
+import { List, Avatar } from "antd";
+export default function RestaurantList({ dataList }) {
+  return (
+    <>
+      <List
+        itemLayout="horizontal"
+        dataSource={dataList}
+        renderItem={item => (
+          <List.Item style={{ paddingLeft: "8px" }}>
+            <List.Item.Meta
+              avatar={<Avatar shape="square" src={item.image} />}
+              title={item.name}
             />
-        </>
-    )
+          </List.Item>
+        )}
+      />
+    </>
+  );
 }
