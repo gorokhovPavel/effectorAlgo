c03793fe7354fa63055ccac17a542b09c3cc9a69
gorokhovPavel <pgorokhov@yandex.ru>
2019-08-06 01:31:08 +0300

    add redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import RestaurantList from "./components/restaurantList/index";
-//import {restaurants} from './components/restaurantList/index.jsx'
 import "antd/dist/antd.css";
 function App() {
   const restaurants = [];
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { routerMiddleware } from "connected-react-router";
+//import reducer from "../reducer";
+//import logger from "../middlewares/logger";
+//import generateId from "../middlewares/generateId";
+//import api from "../middlewares/api";
+//import history from "../history";
+
+const enhancer = applyMiddleware(
+  thunk,
+  routerMiddleware(history)
+  //generateId,
+  //api
+);
+const store = createStore(reducer, enhancer);
+
+//dev only. No need in prod
+window.store = store;
+
+export default store;
