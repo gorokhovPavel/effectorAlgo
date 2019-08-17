82ea0e11288d51f50b3692b898945dacde1096b8
gorokhovPavel <pgorokhov@yandex.ru>
2019-08-18 02:04:11 +0300

    settings in html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import RestaurantList from "./components/restaurantList/index";
+import Test from "./components/test/index";
 import "antd/dist/antd.css";
 function App() {
   const restaurants = [];
   return (
     <>
-      <RestaurantList dataList={restaurants} />
+      <Test />
+      {/* <RestaurantList dataList={restaurants} /> */}
     </>
   );
 }
