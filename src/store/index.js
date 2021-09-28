fe5c7fb8c4bcac562bc86a35ae7bd92a40b2d8b8
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2021-09-28 17:30:12 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,6 @@ import { routerMiddleware } from "connected-react-router";
 import reducer from "./reducers";
 //import logger from "../middlewares/logger";
 //import generateId from "../middlewares/generateId";
-//import api from "../middlewares/api";
 
 const enhancer = applyMiddleware(
   thunk
